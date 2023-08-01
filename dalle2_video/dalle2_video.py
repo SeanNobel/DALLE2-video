89ffd27a85ad443efcb33a9c4a21d9f7a15eb318
SeanNobel <sensho19950503@gmail.com>
2023-08-01 13:56:32 +0900

    bugfixes

--- dalle2_video/dalle2_video.py
+++ dalle2_video/dalle2_video.py
@@ -1946,7 +1946,6 @@ class VideoDecoder(nn.Module):
         learned_variance = self.learned_variance[unet_index]
         b, t, c, h, w, device = *video.shape, video.device
 
-        cprint(video.shape, "yellow")
         assert video.shape[2] == self.channels
         assert h >= target_frame_size and w >= target_frame_size
 
@@ -1990,12 +1989,17 @@ class VideoDecoder(nn.Module):
         # NOTE: NullVQGanVAE does nothing, meaning we're doing pixel space diffusion.
         is_latent_diffusion = not isinstance(vae, NullVQGanVAE)
 
+        _, t_low, c_low, h_low, w_low = lowres_cond_video.shape
+        cprint(lowres_cond_video.shape, "yellow")
+
         vae.eval()
         with torch.no_grad():
             # NOTE: VAE doesn't do temporal information mixing.
             video = vae.encode(video.view(-1, c, h, w)).view(b, t, c, h, w)
             lowres_cond_video = (
-                vae.encode(lowres_cond_video.view(-1, c, h, w)).view(b, t, c, h, w)
+                vae.encode(lowres_cond_video.view(-1, c_low, h_low, w_low)).view(
+                    b, t_low, c_low, h_low, w_low
+                )
                 if exists(lowres_cond_video)
                 else lowres_cond_video
             )
