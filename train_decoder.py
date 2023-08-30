1ffb3156e730387e15280f5b40a3eb65a8d58856
SeanNobel <sensho19950503@gmail.com>
2023-08-30 13:02:57 +0900

    :construction: decoder training script

--- train_decoder.py
+++ train_decoder.py
@@ -7,11 +7,11 @@ from dalle2_video.datasets import CelebVTextDataset
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="celebv-text")
-def train(args: DictConfig) -> None:
+def run(args: DictConfig) -> None:
     logging.getLogger().setLevel(eval(f"logging.{args.log_level}"))
 
     CelebVTextDataset(args)
 
 
 if __name__ == "__main__":
-    train()
+    run()
