3ac1f734271ab4fd4f686fa9846e856af180dfb0
SeanNobel <sensho19950503@gmail.com>
2023-09-29 17:27:23 +0900

    importing bugfix

--- dalle2_video/__init__.py
+++ dalle2_video/__init__.py
@@ -1,2 +1,2 @@
-from dalle2_video.dalle2_video.dalle2_video import Unet3D, VideoDecoder, DALLE2Video
-from dalle2_video.dalle2_video.trainer import VideoDecoderTrainer
+from dalle2_video.dalle2_video import Unet3D, VideoDecoder, DALLE2Video
+from dalle2_video.trainer import VideoDecoderTrainer
